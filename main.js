cc57d6a01b5db3b4ce264fcd88a4990fe76b8928
Matsitsky Serhii <matsitskyss@gmail.com>
2022-10-26 16:05:41 +0300

    font test

---
+++ main.js
@@ -0,0 +1,62 @@
+class FontTestPlatform {
+  canvas;
+  props = {};
+  state = {};
+  refs = {};
+
+  constructor() {
+    window.addEventListener("load", this.init);
+  }
+
+  init = () => {
+    console.log("run");
+    this.app = new PIXI.Application({
+      width: 640,
+      height: 360,
+      backgroundColor: 0x00ff00,
+    });
+    document.body.appendChild(this.app.view);
+
+    this.draw();
+  };
+
+  draw() {
+    const originText = this._initText("Origin");
+    this.app.stage.addChild(originText);
+
+    const scaleContainer = new PIXI.Container();
+    scaleContainer.x = 75;
+    scaleContainer.y = 75;
+    scaleContainer.scale.set(0.7);
+
+    const parentScaledText = this._initText("Parent scaled");
+    scaleContainer.addChild(parentScaledText);
+    this.app.stage.addChild(scaleContainer);
+
+    const scaledText = this._initText("Text scaled");
+    scaledText.x = 125;
+    scaledText.y = 125;
+    scaledText.scale.set(0.7)
+    this.app.stage.addChild(scaledText);
+  }
+
+  _initText(message) {
+    const style = new PIXI.TextStyle({
+      fill: 0xfaf3c1,
+      fontFamily: "SourceSansPro-Bold",
+      fontSize: 32,
+      align: "center",
+      stroke: 0x4c011f,
+      strokeThickness: 5,
+      dropShadow: true,
+      dropShadowColor: 0x4c011f,
+      dropShadowDistance: 3,
+      dropShadowBlur: 12,
+      dropShadowAngle: 0.937,
+    });
+    const text = new PIXI.Text(message, style);
+    return text;
+  }
+}
+
+new FontTestPlatform();
