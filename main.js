43748fc471b588ed05e4514cbf70a17607f5101d
Matsitsky Serhii <matsitskyss@gmail.com>
2022-10-27 17:36:06 +0300

    share double text

--- main.js
+++ main.js
@@ -16,161 +16,67 @@ class FontTestPlatform {
   }
 
   draw() {
-    const originText = this._initText("Origin");
-    this.app.stage.addChild(originText);
+    const style = {
+      fill: 0xfaf3c1,
+      fontFamily: "SourceSansPro-Bold",
+      fontSize: 32,
+      align: "center",
+      stroke: 0x4c011f,
+      strokeThickness: 5,
+      dropShadow: true,
+      dropShadowColor: 0x4c011f,
+      dropShadowDistance: 10,
+      dropShadowBlur: 9,
+      dropShadowAngle: 0.937,
+    };
 
-    const baseText = this._initBaseText("ArialBaseText");
-    baseText.x = 200;
+    const baseText = this._initBaseText("Test double quest Render - One", style);
+    baseText.y = 50;
     this.app.stage.addChild(baseText);
 
-    const noStroke = this._initNoStroke("NoStroke");
-    noStroke.y = 50;
-    this.app.stage.addChild(noStroke);
-
-    const lJBevel = this._initLJBevel("lJBevel");
-    lJBevel.y = 100;
-    this.app.stage.addChild(lJBevel);
-
-    const lJRound = this._initLJBevel("lJRound");
-    lJRound.y = 150;
-    this.app.stage.addChild(lJRound);
-
-    const lJMiter = this._initLJBevel("lJMiter");
-    lJMiter.y = 200;
-    this.app.stage.addChild(lJMiter);
-
-
-    const htmlText = this._initHtmlText("htmlText");
-    htmlText.x = 200;
-    htmlText.y = 200;
-    this.app.stage.addChild(htmlText);
-    
+    const doubleRender1 = this._initDoubleRender("Test double quest Render - Double", style);
+    doubleRender1.y = 100;
+    this.app.stage.addChild(doubleRender1);
   }
 
-  _initHtmlText(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "Arial, sans-serif",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
+  _initBaseText(message, styles) {
+    return new PIXI.Text(message, styles);
   }
 
-  _initLJMiter(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "SourceSansPro-Bold",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      miterLimit: 1,
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
-  }
+  _initDoubleRender(message, styles) {
+    if (styles.dropShadow && styles.strokeThickness) {
+      const root = new PIXI.Container();
 
-  _initLJRound(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "SourceSansPro-Bold",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      lineJoin: "round",
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
-  }
+      let baseStyle = { ...styles };
+      for (const key in baseStyle) {
+        if (key.startsWith("dropShadow")) {
+          delete baseStyle[key];
+        }
+      }
 
-  _initLJBevel(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "SourceSansPro-Bold",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      lineJoin: "bevel",
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
-  }
+      let shadowStyle = { ...styles };
+      for (const key in shadowStyle) {
+        if (key.startsWith("stroke")) {
+          delete shadowStyle[key];
+        }
+      }
 
-  _initBaseText(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "Arial, sans-serif",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
-  }
+      console.log(baseStyle, shadowStyle);
+      const baseText = new PIXI.Text(message, baseStyle);
+      const shadowText = new PIXI.Text(message, shadowStyle);
+      if (baseStyle.strokeThickness !== undefined) {
+        shadowText.x += baseStyle.strokeThickness / 2;
+        shadowText.y += baseStyle.strokeThickness / 2;
+        shadowText.fontWeight += baseStyle.strokeThickness * 100;
+      }
 
-  _initNoStroke(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0x4c011f,
-      fontFamily: "SourceSansPro-Bold",
-      fontSize: 32,
-      align: "center",
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 5,
-      dropShadowBlur: 5,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
-  }
+      root.addChild(baseText);
+      root.addChild(shadowText);
 
-  _initText(message) {
-    const style = new PIXI.TextStyle({
-      fill: 0xfaf3c1,
-      fontFamily: "SourceSansPro-Bold",
-      fontSize: 32,
-      align: "center",
-      stroke: 0x4c011f,
-      strokeThickness: 5,
-      dropShadow: true,
-      dropShadowColor: 0x4c011f,
-      dropShadowDistance: 3,
-      dropShadowBlur: 12,
-      dropShadowAngle: 0.937,
-    });
-    const text = new PIXI.Text(message, style);
-    return text;
+      return root;
+    } else {
+      const text = new PIXI.Text(message, styles);
+      return text;
+    }
   }
 }
